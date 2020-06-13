06e1067e8154b5f2a8046a118be19265b84dbdc1
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-06-13 23:20:35 +0500

    Starter

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/shayan/App/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/shayan/App/Clima-Flutter"
+export "FLUTTER_APPLICATION_PATH=/home/shayan/clima_flutter"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
